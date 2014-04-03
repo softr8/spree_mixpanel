a0ef68b55477149a0aade3fda993068d064964a5
Gustavo Robles <gustavo.robles@crowdint.com>
2014-04-03 11:42:25 -0600

    add tests for mixpanel admin controller

---
+++ spec/controllers/mixpanel_settings_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+describe Spree::Admin::MixpanelSettingsController do
+  stub_authorization!
+  describe '#edit' do
+    it 'assigns a Spree::MixpanelConfiguration and render the view' do
+      spree_get :edit
+      expect(assigns(:config)).to be_an_instance_of(Spree::MixpanelConfiguration)
+      expect(response).to render_template('edit')
+    end
+  end
+
+  describe '#update' do
+    it 'saves the project token preference and redirect to edit settings path' do
+      spree_put :update, { project_token: '123' }
+      expect(Spree::MixpanelConfiguration.new.send(:project_token)).to eq '123'
+      expect(response).to be_redirect
+    end
+
+  end
+
+end
