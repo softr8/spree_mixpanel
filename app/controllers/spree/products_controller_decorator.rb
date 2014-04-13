8d18ccdb9a5ab9bfa8af5bd7beee3896ad52bf4f
Edwin Cruz <softr8@gmail.com>
2014-04-13 13:57:42 -0700

    Adding tracking events to product/taxon show and product index

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,8 +1,17 @@
 Spree::ProductsController.class_eval do
   after_filter :mixpanel_track_search, only: :index
+  after_filter :mixpanel_track_product_show, only: :show
 
   private
   def mixpanel_track_search
-    MixpanelTracker.new.track(spree_current_user.id, 'Product search', { keywords: params[:keywords] })
+    if params[:keywords]
+      MixpanelTracker.new.track(spree_current_user.id, 'Product search', { keywords: params[:keywords] })
+    else
+      MixpanelTracker.new.track(spree_current_user.id, 'Products')
+    end
+  end
+
+  def mixpanel_track_product_show
+    MixpanelTracker.new.track(spree_current_user.id, 'Product Show', @product.name)
   end
 end
---
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -0,0 +1,8 @@
+Spree::TaxonsController.class_eval do
+  after_filter :mixpanel_track_taxon_visit, only: :show
+
+  private
+  def mixpanel_track_taxon_visit
+    MixpanelTracker.new.track(spree_current_user.try(:id), 'Taxon Show', @taxon.name)
+  end
+end
